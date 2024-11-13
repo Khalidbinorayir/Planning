49d85292dce34769310ebeaa91d9d7f8b25af5ec
Khalid bin orayir <159157286+Khalidbinorayir@users.noreply.github.com>
2024-11-13 16:05:47 +0300

    Update velocity_profile_generator.cpp

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -363,7 +363,7 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
     // acceleration/deceleration "a". HINT look at the description of this
     // function. Make sure you handle div by 0
   
-    d = pow((v_f - v_i), 2)/(2*a);  // <- Update
+    d = (v_f * v_f - v_i * v_i)/(2.0*a);  // <- Update
   return d;
      
 }
@@ -386,7 +386,7 @@ double VelocityProfileGenerator::calc_final_speed(const double& v_i,
   // and make v_f = 0 in that case. If the discriminant is inf or nan return
   // infinity
 
-  double disc = pow(v_i , 2) * 2 *a *d;  // <- Fix this
+  double disc = pow(v_i , 2) + 2 *a *d;  // <- Fix this
   if (disc <= 0.0) {
     v_f = 0.0;
   } else if (disc == std::numeric_limits<double>::infinity() ||
